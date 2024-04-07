ddaaa89cc97c8246ef3197b5674afd060d78b34d
Henrisen <56893232+Henrisen@users.noreply.github.com>
2024-04-07 12:05:59 +0200

    Update example.py
    Added Examples for Update 1.1

--- modules/example.py
+++ modules/example.py
@@ -1,8 +1,12 @@
 from Server import ServerModule, Request, Response
 
-server = ServerModule('Example Module')
+module = ServerModule('Example Module')
 
 
-@server.on("example")
+@module.overwrite
+def __init__(self: ServerModule):
+    print(f'This will be shown when the plugin is initilized or reloaded')
+
+@module.on("example")
 def example(request: Request) -> Response:
     return request.fabricate_response(200, 'This is an Example Module')
